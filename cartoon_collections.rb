1df4ea9a9479ab4077ab479048c2be2ccd189386
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-23 20:06:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,11 @@ puts "#{ind}. #{val}"
 end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(arr)
+arr.collect do |i|
+i.capitalize << "!"
+end
 end
 
 def long_planeteer_calls# code an argument here
